1f9f1b70fa0385d6d544f364eff54ac2f98fffbe
Parambir Singh <parambirs@users.noreply.github.com>
2017-02-01 20:58:30 +1100

    Update exercise-slices.go

--- exercise-slices.go
+++ exercise-slices.go
@@ -1,15 +1,13 @@
 package main
 
-import (
-	"golang.org/x/tour/pic"
-)
+import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
 	r := make([][]uint8, dy)
 	for y := 0; y < dy; y++ {
-		r = append(r, make([]uint8, dx))
+		r[y] = make([]uint8, dx)
 		for x := 0; x < dx; x++ {
-			r[y] = append(r[y], uint8(x*y))
+			r[y][x] = uint8(x * y)
 		}
 	}
 	return r
